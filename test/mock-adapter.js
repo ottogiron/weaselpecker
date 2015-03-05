22be0a3cf56ccb23a833e015cecc59dfef5181bb
Otto Giron <otto.giron@xoom.com>
2015-03-05 15:41:44 -0600

    first test

--- test/mock-adapter.js
+++ test/mock-adapter.js
@@ -1,5 +1,25 @@
-var chai = require('chai');
+var should = require('chai').should();
 
-describe('Failed initial test', function(){
-  
+var MockAdapter = require('../lib/mock-adapter');
+
+describe('Mock adapter', function(){
+
+  it('should mock find method', function(done){
+      var Model = new MockAdapter();
+      var fixture = {
+        name: 'Otto',
+        lastName: 'Giron'
+      }
+
+      Model.setResults('find',[fixture]);
+
+      Model.find({})
+      .populate()
+      .exec(function(err, results){
+        results.should.be.an('object');
+        done();
+      });
+
+  });
+
 });
