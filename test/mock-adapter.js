bab135569457588ca7795556e2beefe1bb39160a
Otto Giron <otto.giron@xoom.com>
2015-03-06 10:42:17 -0600

    Added set errors. Fixed a couple of bugs

--- test/mock-adapter.js
+++ test/mock-adapter.js
@@ -1,25 +1,62 @@
 var should = require('chai').should();
+var expect = require('chai').expect;
 
 var MockAdapter = require('../lib/mock-adapter');
-
+var fixture1;
+var fixture2;
+var errorFixture;
 describe('Mock adapter', function(){
 
-  it('should mock find method', function(done){
-      var Model = new MockAdapter();
-      var fixture = {
-        name: 'Otto',
-        lastName: 'Giron'
-      }
+  before(function(){
+    fixture1 = {
+      name: 'Otto',
+      lastName: 'Giron'
+    };
 
-      Model.setResults('find',[fixture]);
+    fixture2 = {
+      name: 'Juan',
+      lastName: 'Perez'
+    };
+
+
+
+  });
+
+  it('should mock find method with results', function(done){
+      var Model = new MockAdapter();
+      Model.setResults('find',[fixture1]);
 
       Model.find({})
       .populate()
       .exec(function(err, results){
         results.should.be.an('object');
-        results.name.should.be.equal(fixture.name);
+        results.name.should.be.equal(fixture1.name);
         done();
       });
+  });
+
+  it('should mock find method with errors', function(done){
+    var Model = new MockAdapter();
+    var err = 'serach returned an error';
+    Model.setErrors('find', [err]);
+    Model.find({})
+    .populate()
+    .exec(function(err, results){
+      expect(err).exists;
+      done();
+    });
+  });
+
+  it('Should return results in the correct order', function(done){
+    var Model = new MockAdapter();
+
+    Model.setResults('find',  [fixture1, fixture2] );
+
+    Model.find({})
+    .exec(function(err, results){
+      results.name.should.be.equal(fixture1.name);
+      done();
+    });
 
   });
 
